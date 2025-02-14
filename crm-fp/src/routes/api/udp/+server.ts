ec3676c0dcb36d2e75ca68492b3a887871069329
joeban <joeanddaneil5@gmail.com>
2025-02-14 16:36:58 +0800

    optimize: udp timeout

--- crm-fp/src/routes/api/udp/+server.ts
+++ crm-fp/src/routes/api/udp/+server.ts
@@ -2,29 +2,52 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import dgram from 'dgram';
 
-const udpServer = dgram.createSocket('udp4');
 const UDP_HOST = 'localhost';
 const UDP_PORT = 41234;
 const TARGET_NUMBER = 10;
 let store = 0;
-let flag = true;
+const udpServer = dgram.createSocket('udp4');
+udpServer.on('error', (err) => {
+	console.error(`伺服器錯誤:\n${err.stack}`);
+	udpServer.close();
+});
+
+udpServer.on('message', (msg, rinfo) => {
+	console.log(`伺服器收到來自 ${rinfo.address}:${rinfo.port} 的消息: ${msg}`);
+});
+
+udpServer.on('listening', () => {
+	const address = udpServer.address();
+	console.log(`伺服器正在監聽 ${address.address}:${address.port}`);
+});
+
+udpServer.bind(UDP_PORT); // 監聽固定的 41234 埠
 
 export const GET: RequestHandler = async () => {
 	return new Response();
 };
 
 export const POST: RequestHandler = async (event) => {
+	let flag = true;
 	const req = event.request;
 	const body = await req.json();
 	if (!body.message) {
 		return json({ error: 'message is required' }, { status: 400 });
 	}
 	const message = Buffer.from(body.message.toString());
+	const udpClient = dgram.createSocket('udp4'); // 使用臨時端口
+
+	// 設置超時機制，確保客戶端在一定時間後自動關閉
+	const timeout = setTimeout(() => {
+		if (udpClient) {
+			udpClient.close();
+			console.log('客戶端超時已關閉');
+		}
+	}, 30000); // 30秒超時
 
 	// 包裝在 Promise 中
 	const sendUdpMessage = () =>
 		new Promise((resolve, reject) => {
-			const udpClient = dgram.createSocket('udp4');
 			udpClient.send(message, UDP_PORT, UDP_HOST, (err) => {
 				if (err) {
 					console.error('UDP 傳輸錯誤:', err);
@@ -41,6 +64,8 @@ export const POST: RequestHandler = async (event) => {
 					console.log('收到訊息:', body.message);
 					resolve('訊息已發送, 目前總和: ' + sum);
 				}
+
+				clearTimeout(timeout);
 			});
 		});
 
@@ -49,24 +74,9 @@ export const POST: RequestHandler = async (event) => {
 		if (!flag) {
 			return json({ message: '已達到目標數字，結束 UDP 傳輸，目前總和：' + store });
 		}
+
 		return json({ message: result });
 	} catch (error) {
 		return json({ error }, { status: 400 });
 	}
 };
-
-udpServer.on('error', (err) => {
-	console.error(`伺服器錯誤:\n${err.stack}`);
-	udpServer.close();
-});
-
-udpServer.on('message', (msg, rinfo) => {
-	console.log(`伺服器收到來自 ${rinfo.address}:${rinfo.port} 的消息: ${msg}`);
-});
-
-udpServer.on('listening', () => {
-	const address = udpServer.address();
-	console.log(`伺服器正在監聽 ${address.address}:${address.port}`);
-});
-
-udpServer.bind(UDP_PORT); // 監聽 41234 埠
