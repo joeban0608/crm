8e9a40f010413652df303abe627868559ca4931b
joeban <joeanddaneil5@gmail.com>
2025-02-19 17:03:18 +0800

    feat: pubsub message to gcp

---
+++ crm-fp/src/routes/api/pubsub/messages/+server.ts
@@ -0,0 +1,59 @@
+import { PubSub } from '@google-cloud/pubsub';
+import type { RequestHandler } from './$types';
+import { json } from '@sveltejs/kit';
+const pubSubClient = new PubSub();
+
+export const GET: RequestHandler = async () => {
+	return new Response();
+};
+export const POST: RequestHandler = async (event) => {
+	// Creates a client; cache this for further use
+	const req = event.request;
+	const body = await req.json();
+	const topicNameOrId = body.topic_name_or_id;
+	const message = body.message;
+
+	if (!body.topic_name_or_id) {
+		return json({ error: 'topic_name_or_id is required' }, { status: 400 });
+	}
+
+	if (!body.message) {
+		return json({ error: 'message is required' }, { status: 400 });
+	}
+
+	// [END pubsub_publish_with_error_handler]
+	// [END pubsub_quickstart_publisher]
+
+	const mid = await publishMessage(topicNameOrId, JSON.stringify(message)).catch((err) => {
+		console.error(err.message);
+		process.exitCode = 1;
+	});
+	if (!mid) {
+		return json({ error: 'publish message failed' }, { status: 500 });
+	}
+
+	return json({ message: 'message published', mid }, { status: 201 });
+};
+
+async function publishMessage(__topicNameOrId: string, __data: string) {
+	// Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+	const dataBuffer = Buffer.from(__data, 'utf-8');
+
+	// Cache topic objects (publishers) and reuse them.
+	const topic = pubSubClient.topic(__topicNameOrId);
+
+	try {
+		const messageId = topic.publishMessage({ data: dataBuffer });
+		console.log(`Message ${messageId} published.`);
+		return messageId;
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error(`Received error while publishing: ${error.message}`);
+		} else {
+			console.error('Received unknown error while publishing');
+		}
+		process.exitCode = 1;
+	}
+}
+
+// __publishMessageToPubSub__('projects/seo-manager-429705/topics/fp-test', 'Hello, World!');
